f35602f84d52c00bbaa2b6b361f237d8ec98d387
Sjors Robroek <s.robroek@vxsan.com>
2026-07-14 10:47:49 +0530

    Relicense from Apache-2.0 to MPL-2.0
    This project is now licensed under the Mozilla Public License 2.0.
    
    Changes:
    - Replace LICENSE with MPL-2.0 text
    - Add MPL-2.0 per-file headers to all source files
    - Update Cargo.toml license field
    - Add CLA and CONTRIBUTING.md
    - Add CLA Assistant GitHub Action workflow
    - Update README license section
    
    All prior releases remain under Apache-2.0. This change applies to
    this commit and all future versions.

--- tests/properties.rs
+++ tests/properties.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Property-based tests of the matcher invariants (SC-007, SC-008).
 //!
 //! The coordinate-math properties this suite once carried (separation
